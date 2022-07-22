99cf55a6a98c4a6e46954c1aa4af791be6a3591d
ohtaekwon <ohtk1119@gmail.com>
2022-07-22 21:14:33 +0900

    chore: 인덱스 파일에 브라우저 라우터 연결

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Suspense } from "react"; // 비동기 처리를 위해 suspense를 사용
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil"; // RecoilRoot는 Recoil 상태값을 사용할 수 있는 Provider를 제공한다.
+import Loader from "./components/Loader";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <RecoilRoot>
+        <Suspense fallback={<Loader />}>
+          <App />
+        </Suspense>
+      </RecoilRoot>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
