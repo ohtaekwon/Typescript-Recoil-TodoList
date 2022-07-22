d562ceebfcd32ccc21324a981add1e4747c87602
ohtaekwon <ohtk1119@gmail.com>
2022-07-22 21:12:43 +0900

    feat: 메인 페이지 생성

---
+++ src/pages/main/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { TodoItemQuery } from "../../state";
+
+export default function MainPage() {
+  const data = useRecoilValue(TodoItemQuery);
+  console.log(data);
+  return (
+    <>
+      <h1>여기는 메인 페이지입니다.</h1>
+      <div>{data.title}</div>
+    </>
+  );
+}
