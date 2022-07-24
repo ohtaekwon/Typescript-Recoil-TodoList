f77abd6fc629cd7e6f54917b64b3470f066d6a09
ohtaekwon <ohtk1119@gmail.com>
2022-07-24 23:58:50 +0900

    fix: 캘린더 컴포넌트 수정

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -42,40 +42,6 @@ const Calendar: React.FC = () => {
     setSelectedDate(date);
   };
 
-  const pad = () =>
-    [...Array(firstDay.getDay()).keys()].map((p: number) => (
-      <TableData key={`pad_${p}`} />
-    )); // 해당 월의 첫째 날 전 pad
-
-  const range = () =>
-    [...Array(lastDay.getDate()).keys()].map((d: number) => {
-      // 1일 부터 마지막 날까지 날짜 표기
-      const thisDay = new Date(year, month, d + 1);
-      const today = new Date();
-
-      return (
-        <TableData key={d} onClick={() => selectDate(thisDay)}>
-          <DisplayDate
-            isSelected={isSameDay(selectedDate, thisDay)}
-            isToday={isSameDay(today, thisDay)}
-          >
-            {new Date(year, month, d + 1).getDate()}
-          </DisplayDate>
-        </TableData>
-      );
-    });
-
-  const render = () => {
-    // table data 를 일주일 단위로 줄바꿈한다.
-    const items = [...pad(), ...range()];
-
-    const weeks = Math.ceil(items.length / 7);
-
-    return [...Array(weeks).keys()].map((week: number) => (
-      <tr key={`week_${week}`}>{items.slice(week * 7, week * 7 + 7)}</tr>
-    ));
-  };
-
   return (
     <Base>
       <Header>
